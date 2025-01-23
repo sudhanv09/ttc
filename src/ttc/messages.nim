99b51f13a454c9a064d6076dbde891731577b13b
sudhanv09 <mystogun125@gmail.com>
2025-01-23 14:40:40 +0800

    implement messages

--- src/ttc/messages.nim
+++ src/ttc/messages.nim
@@ -1,4 +1,4 @@
-import std/[asyncnet]
+import std/[asyncnet, asyncdispatch]
 
 type
   PeerActions* = enum
@@ -13,12 +13,43 @@ type
     pCancel = 8
 
   Message* = object
-    id: PeerActions
-    payload: seq[byte]
+    Id*: PeerActions
+    Payload*: seq[byte]
 
-proc sendRequest(s: AsyncSocket, idx, begin, length: int) = discard
-proc sendChoke(s: AsyncSocket) = discard
-proc sendUnchoke(s: AsyncSocket) = discard
-proc sendInterested(s: AsyncSocket) = discard
-proc sendNotInterested(s: AsyncSocket) = discard
-proc sendHave(s: AsyncSocket, idx: int) = discard
+
+proc serialize(msg: Message): seq[byte] = 
+  let length = msg.Payload.len + 1 # 1 for id
+  var data = newSeq[byte](4 + length)
+
+  # payload length
+  for i in 0..3:
+    data[i] = byte((length shr (8 * (3 - i))) and 0xFF)
+
+  # Action
+  data[4] = msg.Id.byte
+  
+  # Payload
+  for i, b in msg.Payload:
+    data[i+5] = b
+
+  return data
+
+proc sendChoke(s: AsyncSocket) {.async.} = 
+  let msg = Message(Id: pChoke).serialize()
+  await s.send(addr msg[0], msg.len)
+  
+proc sendUnchoke(s: AsyncSocket) {.async.} = 
+  let msg = Message(Id: pUnchoke).serialize()
+  await s.send(addr msg[0], msg.len)
+
+proc sendInterested(s: AsyncSocket) {.async.} = 
+  let msg = Message(Id: pInterested).serialize()
+  await s.send(addr msg[0], msg.len)
+
+proc sendNotInterested(s: AsyncSocket) {.async.} = 
+  let msg = Message(Id: pUninterested).serialize()
+  await s.send(addr msg[0], msg.len)
+
+proc sendHave(s: AsyncSocket, idx: int) {.async.} = 
+  let msg = Message(Id: pHave).serialize()
+  await s.send(addr msg[0], msg.len)
